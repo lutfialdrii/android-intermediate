738388ba148a80c1fa7bd71a85123e682e86ab9a
lutfialdrii <lutfialdripermana@gmail.com>
2024-05-20 22:01:20 +0700

    setup camerax

--- 17.CameraApp/app/src/main/java/com/dicoding/picodiploma/mycamera/CameraActivity.kt
+++ 17.CameraApp/app/src/main/java/com/dicoding/picodiploma/mycamera/CameraActivity.kt
@@ -2,13 +2,26 @@ package com.dicoding.picodiploma.mycamera
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.dicoding.picodiploma.mycamera.databinding.ActivityCameraBinding
 
 class CameraActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = CameraActivity::class.java.simpleName
+    }
+
     private lateinit var binding: ActivityCameraBinding
+
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +41,27 @@ class CameraActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
-        // showCamera
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+            }
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview
+                )
+            } catch (e: Exception) {
+                Toast.makeText(this@CameraActivity, "Something went wrong!", Toast.LENGTH_SHORT)
+                    .show()
+                Log.e(TAG, "startCamera: ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(this))
     }
 
     private fun takePhoto() {
--- 17.CameraApp/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ 17.CameraApp/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.dicoding.picodiploma.mycamera
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -7,21 +10,46 @@ import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.dicoding.picodiploma.mycamera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         private val TAG = MainActivity::class.java.simpleName
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
     }
 
     private lateinit var binding: ActivityMainBinding
     private var currentImageUri: Uri? = null
 
+    //    Launcher Permission
+    private val reqPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+//    Check Permission
+
+    private fun allPermissionGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        Req Permission
+        if (!allPermissionGranted()) {
+            reqPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
 
         binding.galleryButton.setOnClickListener { startGallery() }
         binding.cameraButton.setOnClickListener { startCamera() }
@@ -69,7 +97,7 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun startCameraX() {
-        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+        startActivity(Intent(this, CameraActivity::class.java))
     }
 
     private fun uploadImage() {
