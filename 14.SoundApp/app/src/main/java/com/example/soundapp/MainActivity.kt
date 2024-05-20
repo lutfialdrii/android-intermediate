af113fc5246cc59964d343ab17859f1d448a99ef
lutfialdrii <lutfialdripermana@gmail.com>
2024-05-20 14:19:57 +0700

    set SoundPool

--- 14.SoundApp/app/src/main/java/com/example/soundapp/MainActivity.kt
+++ 14.SoundApp/app/src/main/java/com/example/soundapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.soundapp
 
+import android.media.SoundPool
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sp: SoundPool
+    private var soundId: Int = 0
+    private var spLoaded = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnSound = findViewById<Button>(R.id.btn_sound_pool)
+
+        sp = SoundPool.Builder().setMaxStreams(10).build()
+        sp.setOnLoadCompleteListener { _, _, status ->
+            if (status == 0) {
+                spLoaded = true
+            } else {
+                Toast.makeText(this@MainActivity, "Gagal Load!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        soundId = sp.load(this, R.raw.clinking_glasses, 1)
+
+        btnSound.setOnClickListener {
+            if (spLoaded) {
+                sp.play(soundId, 1f, 1f, 0, 0, 1f)
+            }
         }
     }
 }
