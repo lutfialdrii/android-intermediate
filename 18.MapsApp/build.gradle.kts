156acc25e44e56db8b777a52750969ff63a84564
lutfialdrii <lutfialdripermana@gmail.com>
2024-06-06 11:10:17 +0700

    maps app

--- 18.MapsApp/build.gradle.kts
+++ 18.MapsApp/build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin) apply false
 }
+
+buildscript {
+    dependencies {
+        classpath(libs.secrets.gradle.plugin)
+    }
+}
